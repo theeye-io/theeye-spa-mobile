90c77ecc95f65f405d894f001aacb027fb40301c
tmarcote <tomas.marcote@gmail.com>
2018-07-06 11:23:50 -0300

    added white icon for push notif

---
+++ scripts/push-icon.js
@@ -0,0 +1,9 @@
+var fs = require('fs')
+
+if (!fs.existsSync('platforms/android/res/drawable')) {
+  fs.mkdirSync('platforms/android/res/drawable')
+}
+
+fs.createReadStream('res/drawable/icon_push.png').pipe(
+  fs.createWriteStream('platforms/android/res/drawable/icon_push.png')
+)
