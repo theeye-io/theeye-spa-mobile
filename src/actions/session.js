74a6e55db894046267e3814b06b328bef844035f
tmarcote <tomas.marcote@gmail.com>
2018-09-04 16:03:56 -0300

    added session fixes

--- src/actions/session.js
+++ src/actions/session.js
@@ -3,6 +3,7 @@ import XHR from 'lib/xhr'
 const logger = require('lib/logger')('actions:session')
 import bootbox from 'bootbox'
 import reject from 'lodash/reject'
+import jwtDecode from 'jwt-decode'
 import AnalyticsActions from './analytics'
 
 import { Model as Customer } from 'models/customer'
@@ -53,24 +54,32 @@ module.exports = {
       })
     }
   },
+  verifyAccessToken (token, done) {
+    const isValidFormat = Boolean(token)
+    if (!isValidFormat) { // empty or not set
+      return done( new Error('invalid token format') )
+    }
+
+    let decoded = jwtDecode(token)
+    let date = new Date()
+
+    if ((decoded.exp * 1000) <= date.getTime()) {
+      return done( new Error('session expired') )
+    }
+
+    return done()
+  },
   refreshAccessToken () {
     logger.debug('obtaining new acccess token..')
 
     XHR.send({
       method: 'post',
       url: `${App.config.app_url}/session/refresh`,
-      withCredentials: true,
-      timeout: 5000,
       headers: {
         Accept: 'application/json;charset=UTF-8'
       },
-      done: (data,xhr) => {
+      done: (data, xhr) => {
         App.state.session.access_token = data.access_token
-        //App.state.session.set({
-        //  access_token: data.access_token
-        //},{ silent: true })
-      },
-      fail: (err,xhr) => {
       }
     })
   },
