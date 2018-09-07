cbbecae403825a993fd532b0ffa52c0efea095f6
tmarcote <tomas.marcote@gmail.com>
2018-09-07 15:26:46 -0300

    fix file params

--- src/view/page/dashboard/task/task/exec-task.js
+++ src/view/page/dashboard/task/task/exec-task.js
@@ -22,8 +22,8 @@ const BaseExec = State.extend({
           }
           break
         case 'file':
-          arg.value = arg.value.dataUrl
           arg.renderValue = arg.value.name
+          arg.value = arg.value.dataUrl
           break
         default:
           arg.renderValue = arg.value
