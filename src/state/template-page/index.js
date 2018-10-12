12c53de43893854eb2dde5fb3d0bfa628fadb95c
tmarcote <tomas.marcote@gmail.com>
2018-10-12 13:44:25 -0300

    fix templates creation. add task events

---
+++ src/state/template-page/index.js
@@ -0,0 +1,32 @@
+
+'use strict'
+
+import AmpersandState from 'ampersand-state'
+import { TaskEvents } from './task-event'
+//import gconfig from 'config'
+
+const Resources = require('models/resource').Collection
+const Tasks = require('models/task').Collection
+import { Model as File, Collection as Files } from 'models/file'
+
+// representation of a host template
+export default AmpersandState.extend({
+  collections: {
+    configFiles: Files,
+    configTasks: Tasks,
+    configResources: Resources,
+    configTriggers: TaskEvents
+  },
+  setConfigs (config) {
+    this.configFiles.reset(config.files || [], { parse: true })
+    this.configTasks.reset(config.tasks || [], { parse: true })
+    this.configResources.reset(config.resources || [], { parse: true })
+    this.configTriggers.reset(config.triggers || [], { parse: true })
+  },
+  resetCollection () {
+    this.configFiles.reset([])
+    this.configTasks.reset([])
+    this.configResources.reset([])
+    this.configTriggers.reset([])
+  }
+})
