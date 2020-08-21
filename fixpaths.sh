9ca30f96230f769011542a005ddd67cff8ce388c
Tomas Santiago Marcote <tomas.marcote@gmail.com>
2020-08-21 16:03:43 -0300

    fix paths for android build

--- fixpaths.sh
+++ fixpaths.sh
@@ -12,3 +12,7 @@ maincss=$(find "www/bundles/styles" -name "main.*css")
 sed -i'bkp' -e "s/\/images/images/g" www/bundles/styles/"${maincss##*/}"
 
 sed -i'bkp' -e "s/bundles\/fonts/..\/fonts/g" www/bundles/styles/"${maincss##*/}"
+
+mainjs=$(find "www/bundles/js" -name "main.*js")
+
+sed -i'bkp' -e "s/bundles\/images/android_asset\/www\/bundles\/images/g" www/bundles/js/"${mainjs##*/}"
